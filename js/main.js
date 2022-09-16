731cc1966b6793caf840d733515f740d4cf02193
Felipe Mota <felipe.msrb@gmail.com>
2022-09-16 11:42:04 +0100

    criando função para alterar os dados

--- js/main.js
+++ js/main.js
@@ -2,10 +2,14 @@ const somar = document.querySelector('#somar');
 const subtrair = document.querySelector('#subtrair');
 const braco = document.querySelector('#braco');
 
-somar.addEventListener('click',() => {
-    braco.value = parseInt(braco.value) + 1
-})
+somar.addEventListener('click',() => {manipulaDados('somar')})
 
-subtrair.addEventListener('click',() => {
-    braco.value = parseInt(braco.value) - 1
-})
+subtrair.addEventListener('click',() => {manipulaDados('subtrair')})
+
+function manipulaDados(operacao){
+    if(operacao === 'somar') {
+        braco.value = parseInt(braco.value) + 1
+    } else {
+        braco.value = parseInt(braco.value) - 1
+    }
+}
