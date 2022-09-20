985c677226b828608a506f96649f9a53e3895d98
Felipe Mota <felipe.msrb@gmail.com>
2022-09-20 13:43:10 +0100

    corrigindo bug nos botoes das cores

--- js/main.js
+++ js/main.js
@@ -67,19 +67,3 @@ function atualizaDados(peca) {
     })
 }
 
-function trocaImagem(alterar){
-    const ImagemRobo = document.querySelector('[data-robo]');    
-
-    const cores = [
-        'azul',
-        'branco',
-        'preto',
-        'rosa',
-        'vermelho',
-        'amarelo'
-    ]
-
-    if(cores.includes(alterar)){
-        ImagemRobo.src = `./img/robotron-${alterar}.png`;
-    }
-}
